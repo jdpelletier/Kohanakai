31fdc27896dc2546f4251755723720b7df2d5897
John Pelletier <john.pelletier@ymail.com>
2020-04-12 19:58:42 -1000

    04/12/20

--- ArduinoMultiOnline.py
+++ ArduinoMultiOnline.py
@@ -2,54 +2,61 @@ import serial
 import time
 import datetime
 import Util
-#import chart_studio.plotly as py
-#import plotly.graph_objects as go
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
-
-ser = serial.Serial('/dev/ttyACM0', 9600)
-time.sleep(2)
 
 # Read and record the data
-#values = []
-#data1 =[]
-#data2 =[]
-#data3 = []                    # empty list to store the data
-#t =[]
+values = []
+data1 = []
+data2 = []
+data3 = []
+t = []
 currentDay = datetime.date.today()
 path = Util.FolderCreate(currentDay)
+startTime = datetime.datetime.now()
 
 try:
     while(True):
         if currentDay != datetime.date.today():
             currentDay = datetime.date.today()
             path = Util.FolderCreate(currentDay)
+            startTime = datetime.datetime.now()
+            data1 = []
+            data2 = []
+            data3 = []
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+        time.sleep(5)
         b = ser.readline()         # read a byte string
         string_n = b.decode()      # decode byte string into Unicode
         string = string_n.rstrip() # remove \n and \r
+        print(string)
+        values = string.split()
+        if len(values) != 3 or string[0] != ' ':
+            print("line skipped")
+            continue
+        values[0] = float(values[0])
+        values[1] = float(values[1])
+        values[2] = float(values[2])
+        data1.append(values[0])           # add to the end of data list
+        data2.append(values[1])
+        data3.append(values[2])
+        t = [startTime + datetime.timedelta(minutes=5*i) for i in range(len(data1))]
+        plt.plot(t, data1, 'ro', t, data2, 'bs', t, data3, 'g^')
+        xformatter = mdates.DateFormatter('%H:%M')
+        plt.gcf().axes[0].xaxis.set_major_formatter(xformatter)
+        plt.savefig("TempsToday.jpeg")
+        plt.close()
+        maxes = [np.amax(data1), np.amax(data2), np.amax(data3)]
+        maxTotal = str(np.amax(maxes))
+        string = string + " " + maxTotal
         Util.FileWrite(path, string)
-#        values = string.split()
-#        values[0] = float(values[0])
-#        values[1] = float(values[1])
-#        values[2] = float(values[2])
-#        data1.append(values[0])           # add to the end of data list
-#        data2.append(values[1])
-#        data3.append(values[2])
-#        if i%60 == 0:
-#            t = [datetime.datetime.now() + datetime.timedelta(seconds=i) for i in range(len(data1))]
-#            xy1 = go.Scatter(
-#                x = t,
-#                y = data1
-#                )
-#            xy2 = go.Scatter(
-#                x = t,
-#            xy3 = go.Scatter(
-#                x = t,
-#                y = data3
-#            )
-#            xydata = [xy1, xy2, xy3]
-#            first_plot_url = py.plot(xydata, filename = 'Temps', auto_open=False)
-        time.sleep(300)            # wait 5 minutes
+        Util.HandlePlot(path, "TempsToday.jpeg")
+        Util.HTMLWrite(string)
+        ser.close()
+        time.sleep(295)            # wait 5 minutes
 except KeyboardInterrupt:
     print('Script ended')
 
--- Util.py
+++ Util.py
@@ -1,12 +1,16 @@
 import os
 import datetime
+import shutil
+
+def NowString():
+    today = datetime.datetime.now()
+    time = today.time().strftime('%H:%M:%S')
+    return time
 
 def FileWrite(filepath, data):
     filename = os.path.join(str(filepath), "dataFile.txt")
     with open(filename, 'a') as f:
-        today = datetime.datetime.now()
-        time = today.time().strftime('%H:%M:%S')
-        f.write(time)
+        f.write(NowString())
         f.write(data)
         f.write("\n")
         f.close()
@@ -25,3 +29,38 @@ def FolderCreate(today):
     else:
         print ("Successfully created the directory %s " % path)
     return path
+
+def HandlePlot(path, image):
+    test_dir = path
+    image_dir = "/var/www/html"
+    shutil.copy(image, test_dir)
+    shutil.copy(image, image_dir)
+
+def HTMLWrite(data):
+    filename = "/var/www/html/index.html"
+    currentDay = datetime.date.today()
+    dayString = currentDay.isoformat()
+    data = data.split()
+    with open(filename, 'w+') as f:
+        text = '''
+            <!DOCTYPE html>
+            <html>
+            <header><title>Tank Temps</title></header>
+            <body>
+            <h1>Tank Temps on %s</h1>
+            <p>Max temp for today:</p>
+            <ul>
+                <li>%s</li>
+            </ul>
+            <p>Temps at %s</p>
+            <ul>
+              <li>Meter 1: %s</li>
+              <li>Meter 2: %s</li>
+              <li>Meter 3: %s</li>
+            </ul>
+            <img src="TempsToday.jpeg" ALT="Temp Plot">
+            </body>
+            </html>
+            ''' % (dayString, data[3], NowString(), data[0], data[1], data[2])
+        f.write(text)
+        f.close()
